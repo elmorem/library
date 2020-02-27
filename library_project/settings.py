f574920750231b21d1e54cc9cc5077daa51287fd
elmorem <elmorem@gmail.com>
2020-02-27 12:26:35 -0800

    added api app

--- library_project/settings.py
+++ library_project/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+# 3rd party
+'rest_framework',
+
+
 #local
-    'books.apps.BooksConfig'
+    'books.apps.BooksConfig',
+    'api.apps.ApiConfig',
 
 ]
 
